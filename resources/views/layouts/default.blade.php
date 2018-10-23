96e160ee71d7f35e836c2b4d72579783a8e50088
XH <cgxiaohui@163.com>
2018-10-23 14:50:12 +0800

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYOE html>
+<html>
+    <head>
+        <title>
+            @yield('title','NewSample App')
+        </title>
+    </head>
+    <body>
+        @yield("content")
+    </body>
+</html>
